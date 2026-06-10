15986d47cd6c56b384d6c4f6d8d6bd463e670a8f
kabuspl <me@kabus.ovh>
2026-06-10 23:54:26 +0200

    let's use it actually

---
+++ dot_local/share/konsole/private_Home Server.profile
@@ -0,0 +1,8 @@
+[Appearance]
+ColorScheme=Breeze
+
+[General]
+Command=/bin/ssh 192.168.1.116
+Icon=computer-laptop-symbolic
+Name=Home Server
+Parent=FALLBACK/
--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,7 +1,14 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    bind alt-backspace backward-kill-word
     thefuck --alias | source
 	set -g fish_greeting
-	pfetch
+	if set -q KONSOLE_DBUS_WINDOW
+        fastfetch
+    else
+        fastfetch -c ~/.config/fastfetch/zed
+	end
 	atuin init fish --disable-up-arrow | source
+    set -xU MANPAGER 'less -R --use-color -Dd+r -Du+b'
+    set -xU MANROFFOPT '-P -c'
 end
